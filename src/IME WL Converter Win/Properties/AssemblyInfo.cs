7b18d5407d2fbd83186e06a1fbe2661c2bce0615
Devin <studyzy@gmail.com>
2019-06-08 00:29:57 +0800

    upgrade version to 2.6.0

--- src/IME WL Converter Win/Properties/AssemblyInfo.cs
+++ src/IME WL Converter Win/Properties/AssemblyInfo.cs
@@ -38,5 +38,5 @@ using System.Runtime.InteropServices;
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.5.0.0")]
-[assembly: AssemblyFileVersion("2.5.0.0")]
+[assembly: AssemblyVersion("2.6.0.0")]
+[assembly: AssemblyFileVersion("2.6.0.0")]
