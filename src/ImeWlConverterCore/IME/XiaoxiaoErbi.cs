3b22a6bbe7686e886d14fc547df65ee3c5599551
studyzy <studyzy@gmail.com>
2020-12-04 14:38:14 +0800

    add GPLv3 header

--- src/ImeWlConverterCore/IME/XiaoxiaoErbi.cs
+++ src/ImeWlConverterCore/IME/XiaoxiaoErbi.cs
@@ -1,4 +1,21 @@
-﻿using System.Collections.Generic;
+﻿/*
+ *   Copyright © 2009-2020 studyzy(深蓝,曾毅)
+
+ *   This program "IME WL Converter(深蓝词库转换)" is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using System.Collections.Generic;
 using System.Text;
 using Studyzy.IMEWLConverter.Entities;
 
