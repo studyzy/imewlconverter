5fd3bee27165d3d0a5e0c70a8e763df5fc33ef80
YFdyh000 <yfdyh000@gmail.com>
2022-03-23 15:03:13 +0800

    fixes

--- src/ImeWlConverterCoreTest/HelperTest/FileOperationTest.cs
+++ src/ImeWlConverterCoreTest/HelperTest/FileOperationTest.cs
@@ -27,10 +27,10 @@ namespace Studyzy.IMEWLConverter.Test.HelperTest
 {
     class FileOperationTest
     {
-        [TestCase("Test\\u8nobomzy.txt","UTF-8")]
-        [TestCase("Test\\luna_pinyin_export.txt", "UTF-8")]
-        [TestCase("Test\\gbzy.txt", "GB18030")]
-        [TestCase("Test\\QQPinyin.txt", "Unicode")]
+        [TestCase("Test/u8nobomzy.txt","UTF-8")]
+        [TestCase("Test/luna_pinyin_export.txt", "UTF-8")]
+        [TestCase("Test/gbzy.txt", "GB18030")]
+        [TestCase("Test/QQPinyin.txt", "Unicode")]
         public void TestGetFileEncoding(string path,string encoding)
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
