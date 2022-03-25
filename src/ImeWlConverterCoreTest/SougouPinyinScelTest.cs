e11f16e3ad1b906d769d49369a6461a2b49aae89
YFdyh000 <yfdyh000@gmail.com>
2022-03-25 23:38:57 +0800

    add TestLatestScelOnWeb

--- src/ImeWlConverterCoreTest/SougouPinyinScelTest.cs
+++ src/ImeWlConverterCoreTest/SougouPinyinScelTest.cs
@@ -16,6 +16,9 @@
  */
 
 using System;
+using System.IO;
+using System.Net;
+using System.Text;
 using NUnit.Framework;
 using Studyzy.IMEWLConverter.Entities;
 using Studyzy.IMEWLConverter.IME;
@@ -91,5 +94,28 @@ namespace Studyzy.IMEWLConverter.Test
             Assert.AreEqual(info["Info"], "包含唐诗300首的所有诗人、诗名、诗句。");
             Assert.True(info["Sample"].Contains("张九龄 侧见双翠鸟"));
         }
-   }
+
+        [Ignore("按需使用。下载1MB，用时1秒")]
+        [TestCase("https://pinyin.sogou.com/d/dict/download_cell.php?id=4&name=%E7%BD%91%E7%BB%9C%E6%B5%81%E8%A1%8C%E6%96%B0%E8%AF%8D%E3%80%90%E5%AE%98%E6%96%B9%E6%8E%A8%E8%8D%90%E3%80%91&f=detail")]
+        public void TestLatestScelOnWeb(string url)
+        {
+            var filePath = Path.GetTempFileName();
+            WebClient dl = new WebClient();
+            dl.DownloadFile(url, filePath);
+
+            var info = SougouPinyinScel.ReadScelInfo(GetFullPath(filePath));
+            Assert.That(info, Is.Not.Null.And.Not.Empty);
+            foreach (var item in info)
+                TestContext.WriteLine(item.Key + ": " + item.Value);
+            Assert.Greater(info["CountWord"], "10000");
+            Assert.AreEqual(info["Name"], "网络流行新词【官方推荐】");
+            Assert.AreEqual(info["Type"], "北京");
+            Assert.AreEqual(info["Info"], "搜狗搜索自动生成的流行新词，每周更新。");
+            Assert.That(info["Sample"], Is.Not.Null.And.Not.Empty);
+
+            var lib = importer.Import(GetFullPath(filePath));
+            Assert.Greater(lib.Count, 0);
+            Assert.That(lib[0].Word, Is.Not.Null.And.Not.Empty);
+        }
+    }
 }
