8bbc702017bb85cdab99ac0b32a649a9bda04486
YFdyh000 <yfdyh000@gmail.com>
2022-03-25 23:16:52 +0800

    add ReadScelInfo and SougouPinyinScelTest

---
+++ src/ImeWlConverterCoreTest/SougouPinyinScelTest.cs
@@ -0,0 +1,95 @@
+﻿/*
+ *   Copyright © 2022 yfdyh000
+
+ *   This program "IME WL Converter(深蓝词库转换)" is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using System;
+using NUnit.Framework;
+using Studyzy.IMEWLConverter.Entities;
+using Studyzy.IMEWLConverter.IME;
+
+namespace Studyzy.IMEWLConverter.Test
+{
+    [TestFixture]
+    internal class SougouPinyinScelTest : BaseTest
+    {
+        
+        [OneTimeSetUp]
+        public override void InitData()
+        {
+            importer = new SougouPinyinScel();
+        }
+
+        protected override string StringData
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        [TestCase]
+        public void TestImportLine()
+        {
+            Assert.Catch(() =>
+            {
+                importer.ImportLine("test");
+            }, "Scel格式是二进制文件，不支持流转换");
+        }
+        [Ignore("较慢，按需启用")]
+        [TestCase("诗词名句大全.scel")]
+        public void TestImportBigScel(string filePath)
+        {
+            var lib = importer.Import(GetFullPath(filePath));
+            Assert.Greater(lib.Count, 0);
+            Assert.That(lib[0].Word, Is.Not.Null.And.Not.Empty);
+
+            Assert.AreEqual(lib.Count, 342179);
+            Assert.AreEqual(lib[0].CodeType, Studyzy.IMEWLConverter.Entities.CodeType.Pinyin);
+            Assert.AreEqual(lib[0].IsEnglish, false);
+            Assert.AreEqual(lib[0].PinYinString, "a'cheng'yi'wen'you'bi'duan");
+            Assert.AreEqual(lib[0].Rank, 0);
+            Assert.AreEqual(lib[0].SingleCode, "a");
+            Assert.AreEqual(lib[0].Word, "阿秤亦闻有笔端");
+        }
+        [TestCase("唐诗300首【官方推荐】.scel")]
+        public void TestImportSmallScel(string filePath)
+        {
+            var lib = importer.Import(GetFullPath(filePath));
+            Assert.Greater(lib.Count, 0);
+            Assert.That(lib[0].Word, Is.Not.Null.And.Not.Empty);
+
+            Assert.AreEqual(lib.Count, 3563);
+            Assert.AreEqual(lib[0].CodeType, Studyzy.IMEWLConverter.Entities.CodeType.Pinyin);
+            Assert.AreEqual(lib[0].IsEnglish, false);
+            Assert.AreEqual(lib[0].PinYinString, "ai'jiang'tou");
+            Assert.AreEqual(lib[0].Rank, 0);
+            Assert.AreEqual(lib[0].SingleCode, "ai");
+            Assert.AreEqual(lib[0].Word, "哀江头");
+            Assert.AreEqual(lib[0].WubiCode, null);
+        }
+        [TestCase("唐诗300首【官方推荐】.scel")]
+        public void TestListScelInfo(string filePath)
+        {
+            var info = SougouPinyinScel.ReadScelInfo(GetFullPath(filePath));
+            Assert.That(info, Is.Not.Null.And.Not.Empty);
+            foreach (var item in info)
+                TestContext.WriteLine(item.Key + ": " + item.Value);
+
+            Assert.AreEqual(info["CountWord"], "3563");
+            Assert.AreEqual(info["Name"], "唐诗300首【官方推荐】");
+            Assert.AreEqual(info["Type"], "诗词歌赋");
+            Assert.AreEqual(info["Info"], "包含唐诗300首的所有诗人、诗名、诗句。");
+            Assert.True(info["Sample"].Contains("张九龄 侧见双翠鸟"));
+        }
+   }
+}
