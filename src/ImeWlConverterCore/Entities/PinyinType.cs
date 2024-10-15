159ca5883d0b29a18dced9a83318ff9cc5ec4f4e
nopdan <xci@live.com>
2024-10-15 11:49:22 +0800

    fix: 校对双拼映射表

--- src/ImeWlConverterCore/Entities/PinyinType.cs
+++ src/ImeWlConverterCore/Entities/PinyinType.cs
@@ -25,48 +25,48 @@ namespace Studyzy.IMEWLConverter.Entities
         FullPinyin = 0,
 
         /// <summary>
-        /// 微软双拼
-        /// </summary>
-        MsShuangpin = 1,
-
-        /// <summary>
         /// 小鹤双拼
         /// </summary>
-        XiaoheShuangpin = 2,
-
-        /// <summary>
-        /// 智能ABC
-        /// </summary>
-        SmartABCShuangpin = 3,
+        XiaoheShuangpin = 1,
 
         /// <summary>
         /// 自然码
         /// </summary>
-        NatureCode = 4,
+        NatureCode = 2,
 
         /// <summary>
-        /// 拼音加加
+        /// 微软双拼
         /// </summary>
-        PinyinJiajia = 5,
+        MsShuangpin = 3,
 
         /// <summary>
-        /// 星空键道
+        /// 智能ABC
         /// </summary>
-        XingkongJiandao = 6,
-
-        /// <summary>
-        /// 大牛双拼
-        /// </summary>
-        DaniuShuangpin = 7,
-
-        /// <summary>
-        /// 小浪双拼
-        /// </summary>
-        XiaolangShuangpin = 8,
+        SmartABCShuangpin = 4,
 
         /// <summary>
         /// 紫光拼音
         /// </summary>
-        ZiguangShuangpin = 9,
+        ZiguangShuangpin = 5,
+
+        /// <summary>
+        /// 拼音加加
+        /// </summary>
+        PinyinJiajia = 6,
+
+        /// <summary>
+        /// 星空键道
+        /// </summary>
+        XingkongJiandao = 7,
+
+        /// <summary>
+        /// 大牛双拼
+        /// </summary>
+        DaniuShuangpin = 8,
+
+        /// <summary>
+        /// 小浪双拼
+        /// </summary>
+        XiaolangShuangpin = 9,
     }
 }
