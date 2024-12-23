928d3157174dc19ca5089ed7553d070fc114191a
nopdan <xci@live.com>
2024-12-23 19:58:19 +0800

    chore: bump version to 3.2.0

--- src/ImeWlConverterCore/ConstantString.cs
+++ src/ImeWlConverterCore/ConstantString.cs
@@ -22,7 +22,7 @@ namespace Studyzy.IMEWLConverter
     /// </summary>
     public class ConstantString
     {
-        public const string VERSION = "3.1.1.0";
+        public const string VERSION = "3.2.0.0";
 
         public const string BAIDU_SHOUJI = "百度手机或Mac版百度拼音";
         public const string BAIDU_SHOUJI_ENG = "百度手机英文";
